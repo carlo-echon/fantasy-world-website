9913559b6c687bf4581532e2116191c2ee120b35
Carlo Echon <carloechon1@gmail.com>
2024-08-05 21:41:46 +0800

    Added Modal
    Imported NextUI Modal and other necessary components, for creating a form to add history entries, through the front-end.

--- app/history/page.js
+++ app/history/page.js
@@ -1,6 +1,65 @@
+"use client"
+
 import HistoryWidget from "./ui/historyWidget";
+import {
+    Modal, 
+    ModalContent, 
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter,
+  } from "@nextui-org/modal";
+import {Input, Textarea} from "@nextui-org/input";
+import {Button} from "@nextui-org/button";
+import {useDisclosure} from '@nextui-org/use-disclosure';
+
+import { getAllHistory, postHistory } from "./services/historyService";
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/navigation';
+
+
 
 export default function History() {
+    const router = useRouter();
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const [history, setHistory] = useState([]);
+    const [error, setError] = useState(null);
+    const [historyDate, setHistoryDate] = useState("");
+    const [historyEvent, setHistoryEvent] = useState("");
+    const [historyDescription, setHistoryDesc] = useState("");
+
+    const handlePost = async () => {
+      try{
+        console.log("In handlePost");
+        const newHistoryEntry = {historyDate, historyEvent, historyDescription};
+        console.log(newHistoryEntry);
+        await postHistory(newHistoryEntry);
+        location.reload() //Not pretty and does a hard reload but works for now. Might need to make a custom refresh function since router.refresh works serverside. 
+      } catch(err){
+        setError(err.message);
+      }
+      
+    }
+
+    useEffect(() => {
+      const getHistory = async () => {
+        try {
+          const result = await getAllHistory();
+          console.log(result.data);
+          setHistory(result.data);
+        } catch (err) {
+          setError(err.message);
+          console.log(error)
+        }
+      };
+  
+      getHistory();
+    }, []);
+
+    useEffect(() => {
+      console.log('History state updated:', history); // Log the updated history state
+    }, [history]);
+    
+
     return(
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="w-full">
@@ -8,19 +67,91 @@ export default function History() {
                     <h1 className="text-2xl">Chronus Grimoire: History of the Known World</h1>
                 </div>
                 <div className="flex justify-end">
-                    <div className="border rounded-lg border-white text-center bg-green-700 hover:bg-green-800 px-4 py-2">Add</div>
+                  <>
+                  <Button className="border rounded-lg border-white text-center bg-green-700 hover:bg-green-800 px-4 py-2"
+                    onClick={onOpen}>
+                        Add
+                  </Button>
+            
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="border-white border rounded bg-black text-white" backdrop={"blur"}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Add History Entry</ModalHeader>
+              <ModalBody>
+              <Input 
+                type="text" 
+                label="Date" 
+                labelPlacement="outside" 
+                placeholder="Enter the Date"
+                onChange={(e) => {
+                  setHistoryDate(e.target.value);
+                }}
+                classNames={{
+                  label: "text-white/90",
+                  input: [
+                    "bg-default-700/50",
+                    "dark:text-white/60",
+                    "dark:placeholder:text-white/60",
+                  ],
+                  innerWrapper: "bg-transparent",
+                  inputWrapper: [
+                    "bg-default/60",
+
+                    "hover:bg-default-200/70",
+                    "dark:hover:bg-default/70",
+                    "group-data-[focus=true]:bg-default-200/50",
+                    "dark:group-data-[focus=true]:bg-default/60",
+                    "!cursor-text",
+                  ],
+                }}
+              />
+                <Input 
+                type="text" 
+                label="Event" 
+                labelPlacement={"outside"} 
+                color="lengua" 
+                placeholder="Enter the Event"
+                onChange={(e) => {
+                  setHistoryEvent(e.target.value);
+                }}/>
+                <Textarea 
+                label="Description"
+                placeholder="Enter your description" 
+                className="w-full"
+                onChange={(e) => {
+                  setHistoryDesc(e.target.value);
+                }}/>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onClick={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onClick={handlePost}>
+                  Add
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>    
+                  </>
+                      
                 </div>
             </div>
 
             <div className="grid text-center grid-cols-4 col-span-4 gap-3">
-                <HistoryWidget 
-                    date = {"17th of Evermoor"}
-                    event = {"Founding of the Sorcerer Kingdom of Thayen"}
-                />
-                <HistoryWidget 
-                    date = {"Beginning of Time"}
-                    event = {"The Conflux of Stars"}
-                />
+                {history.map((entry) => 
+                (
+                  <HistoryWidget 
+                  date={entry.historydate}
+                  event={entry.historyevent}
+                  description={entry.historydescription}
+                  historyid = {entry.historyid}
+                  key={entry.historyid || index} 
+                  />
+                )
+                )}
             </div>
             
             <div className="border rounded-lg border-white text-center text-md p-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+const {nextui} = require("@nextui-org/theme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
+    "./node_modules/@nextui-org/theme/dist/components/modal.js",
+    "./node_modules/@nextui-org/theme/dist/components/input.js",
   ],
   theme: {
     extend: {
@@ -12,7 +19,42 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        zinc: "#FFFFFF"
+
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#000000", // or DEFAULT
+            foreground: "#18181b", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#FFFFFF",
+              DEFAULT: "#18181b",
+              background: "#f31260",
+            },
+            blue: {
+              50: "#e6f1fe",
+              100: "#cce3fd",
+              200: "#99c7fb",
+              300: "#66aaf9",
+              400: "#338ef7",
+              500: "#006FEE",
+              600: "#005bc4",
+              700: "#004493",
+              800: "#002e62",
+              900: "#001731",
+            },
+            white: {
+              90: "#FAFAFA",
+            }
+          },
+        },
+      }
+  })],
 };
