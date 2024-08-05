e8d1f5f7ee127bf0b13e1f077d1dfaf0614ae452
Carlo Echon <carloechon1@gmail.com>
2024-08-05 21:42:31 +0800

    Back-end connection
    This connects the back-end CRUD API to the front-end allowing the user to add entries to the history page.

---
+++ app/helpers/apiHelper.js
@@ -0,0 +1,5 @@
+export const buildHeaders = (args) => {
+    return {
+      'Content-Type': 'application/json',
+    }
+  }
---
+++ app/history/services/historyService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { buildHeaders } from "@/app/helpers/apiHelper";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+
+
+const handleError = (error) => {
+    if (error.response) {
+      throw new Error(`Error: ${error.response.data.message || error.response.statusText}`);
+    } else if (error.request) {
+      throw new Error('Error: No response received from server.');
+    } else {
+      throw new Error(`Error: ${error.message}`);
+    }
+  };
+
+export const getAllHistory = () => {
+    return axios.get(
+        `${API_BASE_URL}/history`
+    )
+}
+
+
+export const postHistory = (args) => {
+    return axios.post(
+      `${API_BASE_URL}/history`,
+      args,
+      {
+        headers: buildHeaders()
+      }
+    )
+    
+}
+
+export const deleteOneHistory = (historyid) => {
+  return axios.delete(
+    `${API_BASE_URL}/history/${historyid}`
+  )
+}
+
+export const getOneHistory = (historyid) => {
+  return axios.get(
+    `${API_BASE_URL}/history/${historyid}`
+  )
+}
+
+
